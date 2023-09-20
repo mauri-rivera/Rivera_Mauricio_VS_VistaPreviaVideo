48507e9faf12252ff7c11405adf147c74d15d18a
mauri-rivera <mauri.rivera@outlook.com>
2023-09-19 22:05:14 -0300

    Se eliminaron archivos

--- js/script.js
+++ js/script.js
@@ -1,122 +1,19 @@
-import * as fs from 'node:fs';
 
-const cors = require('cors');
-const app = express();
-app.use(cors({ 
-    origin: 'http://localhost:5500'
-}));
-
-let contenidoVideo = "";
-
-/*function identificarVideo(elemento) {
+function mostrarId(elemento) {
+    let contenidoVideo = "";
     let identificador = elemento.id;
-    return identificador;
-}*/
-
-document.getElementsByClassName("video").onclick = function () {
-    let identificador = document.getElementsByClassName("video").id;
-    console.log(identificador);
-    /*document.getElementById(identificador).onclick = function () {
-        if (document.getElementById("pantallaVideo").hasChildNodes()) {
-
-            document.getElementById("videoActual").remove();
-            contenidoVideo = document.createElement("VIDEO");
-            contenidoVideo.setAttribute("class", "muestraVideo");
-            contenidoVideo.setAttribute("id", "videoActual");
-
-            if (contenidoVideo.canPlayType("video/mp4")) {
-                //let src = `videos/${identificador}/`;
-                let src = `videos/${identificador}/`
-                let ubicacion = leerCarpeta(src);
-                contenidoVideo.setAttribute("src", `videos/${identificador}/${ubicacion}`);
-            } else {
-                contenidoVideo.setAttribute("src", "movie.ogg");
-            }
-
-            contenidoVideo.setAttribute("width", "720");
-            contenidoVideo.setAttribute("height", "400");
-            contenidoVideo.setAttribute("controls", "controls");
-            document.getElementById("pantallaVideo").appendChild(contenidoVideo);
-        }
-        else {
-            document.getElementById("fondoNeutro").remove();
-            contenidoVideo = document.createElement("VIDEO");
-            contenidoVideo.setAttribute("class", "muestraVideo");
-            contenidoVideo.setAttribute("id", "videoActual");
-
-            if (contenidoVideo.canPlayType("video/mp4")) {
-                let src = `videos/${identificador}/`;
-                let ubicacion = leerCarpeta(src);
-                contenidoVideo.setAttribute("src", `videos/${identificador}/${ubicacion}`);
-            } else {
-                contenidoVideo.setAttribute("src", "movie.ogg");
-            }
-
-            contenidoVideo.setAttribute("width", "720");
-            contenidoVideo.setAttribute("height", "400");
-            contenidoVideo.setAttribute("controls", "controls");
-            document.getElementById("pantallaVideo").appendChild(contenidoVideo);
-        }
-    }*/
-}
-
-function leerCarpeta(src) {
-    const files = fs.readdirSync(src);
-
-    let archivo = files.toString();
-    return archivo;
-}
-
-
-
-
-
-/*document.getElementById("video1").onclick = function () {
+ 
     if (document.getElementById("pantallaVideo").hasChildNodes()) {
+
         document.getElementById("videoActual").remove();
         contenidoVideo = document.createElement("VIDEO");
         contenidoVideo.setAttribute("class", "muestraVideo");
         contenidoVideo.setAttribute("id", "videoActual");
 
         if (contenidoVideo.canPlayType("video/mp4")) {
-            contenidoVideo.setAttribute("src", "videos/pexels-yogendra-singh-15135149 (Original).mp4");
-        } else {
-            contenidoVideo.setAttribute("src", "movie.ogg");
-        }
-
-        contenidoVideo.setAttribute("width", "720");
-        contenidoVideo.setAttribute("height", "400");
-        contenidoVideo.setAttribute("controls", "controls");
-        document.getElementById("pantallaVideo").appendChild(contenidoVideo);
-    }
-    else{
-        document.getElementById("fondoNeutro").remove();
-        contenidoVideo = document.createElement("VIDEO");
-        contenidoVideo.setAttribute("class", "muestraVideo");
-        contenidoVideo.setAttribute("id", "videoActual");
-
-        if (contenidoVideo.canPlayType("video/mp4")) {
-            contenidoVideo.setAttribute("src", "videos/pexels-yogendra-singh-15135149 (Original).mp4");
-        } else {
-            contenidoVideo.setAttribute("src", "movie.ogg");
-        }
-
-        contenidoVideo.setAttribute("width", "720");
-        contenidoVideo.setAttribute("height", "400");
-        contenidoVideo.setAttribute("controls", "controls");
-        document.getElementById("pantallaVideo").appendChild(contenidoVideo);
-    }
-}
-
-document.getElementById("video2").onclick = function () {
-    if (document.getElementById("pantallaVideo").hasChildNodes()) {
-        document.getElementById("videoActual").remove();
-        contenidoVideo = document.createElement("VIDEO");
-        contenidoVideo.setAttribute("class", "muestraVideo");
-        contenidoVideo.setAttribute("id", "videoActual");
-
-        if (contenidoVideo.canPlayType("video/mp4")) {
-            contenidoVideo.setAttribute("src", "videos/pexels-ricardo-perez-18092718 (1080p).mp4");
+            let src = `videos/${identificador}`;
+            let ubicacion = encontrarUbicacion(identificador);
+            contenidoVideo.setAttribute("src", `${src}/${ubicacion}`);
         } else {
             contenidoVideo.setAttribute("src", "movie.ogg");
         }
@@ -133,7 +30,9 @@ document.getElementById("video2").onclick = function () {
         contenidoVideo.setAttribute("id", "videoActual");
 
         if (contenidoVideo.canPlayType("video/mp4")) {
-            contenidoVideo.setAttribute("src", "videos/pexels-ricardo-perez-18092718 (1080p).mp4");
+            let src = `videos/${identificador}`;
+            let ubicacion = encontrarUbicacion(identificador);
+            contenidoVideo.setAttribute("src", `${src}/${ubicacion}`);
         } else {
             contenidoVideo.setAttribute("src", "movie.ogg");
         }
@@ -145,76 +44,25 @@ document.getElementById("video2").onclick = function () {
     }
 }
 
-document.getElementById("video3").onclick = function () {
-    if (document.getElementById("pantallaVideo").hasChildNodes()) {
-        document.getElementById("videoActual").remove();
-        contenidoVideo = document.createElement("VIDEO");
-        contenidoVideo.setAttribute("class", "muestraVideo");
-        contenidoVideo.setAttribute("id", "videoActual");
-
-        if (contenidoVideo.canPlayType("video/mp4")) {
-            contenidoVideo.setAttribute("src", "videos/pexels-emanuel-panarello-18126060 (Original).mp4");
-        } else {
-            contenidoVideo.setAttribute("src", "movie.ogg");
+function encontrarUbicacion(id) {
+    try{
+        let ruta = "";
+        switch (id) {
+            case "video1":
+                ruta = "video-(1080p).mp4";
+                break;
+            case "video2":
+                ruta = "pexels-yogendra-singh-15135149-(Original).mp4";
+                break;
+            case "video3":
+                ruta = "pexels-ricardo-perez-18092718-(1080p).mp4";
+                break;
+            case "video4":
+                ruta = "pexels-swapnil-sharma-14916652-(1080p).mp4";
+                break;
         }
 
-        contenidoVideo.setAttribute("width", "720");
-        contenidoVideo.setAttribute("height", "400");
-        contenidoVideo.setAttribute("controls", "controls");
-        document.getElementById("pantallaVideo").appendChild(contenidoVideo);
+        return ruta;
     }
-    else {
-        document.getElementById("fondoNeutro").remove();
-        contenidoVideo = document.createElement("VIDEO");
-        contenidoVideo.setAttribute("class", "muestraVideo");
-        contenidoVideo.setAttribute("id", "videoActual");
-
-        if (contenidoVideo.canPlayType("video/mp4")) {
-            contenidoVideo.setAttribute("src", "videos/pexels-emanuel-panarello-18126060 (Original).mp4");
-        } else {
-            contenidoVideo.setAttribute("src", "movie.ogg");
-        }
-
-        contenidoVideo.setAttribute("width", "720");
-        contenidoVideo.setAttribute("height", "400");
-        contenidoVideo.setAttribute("controls", "controls");
-        document.getElementById("pantallaVideo").appendChild(contenidoVideo);
-    }
+    catch(err){}
 }
-
-document.getElementById("video4").onclick = function () {
-    if (document.getElementById("pantallaVideo").hasChildNodes()) {
-        document.getElementById("videoActual").remove();
-        contenidoVideo = document.createElement("VIDEO");
-        contenidoVideo.setAttribute("class", "muestraVideo");
-        contenidoVideo.setAttribute("id", "videoActual");
-
-        if (contenidoVideo.canPlayType("video/mp4")) {
-            contenidoVideo.setAttribute("src", "videos/pexels-swapnil-sharma-14916652 (1080p).mp4");
-        } else {
-            contenidoVideo.setAttribute("src", "movie.ogg");
-        }
-
-        contenidoVideo.setAttribute("width", "720");
-        contenidoVideo.setAttribute("height", "400");
-        contenidoVideo.setAttribute("controls", "controls");
-        document.getElementById("pantallaVideo").appendChild(contenidoVideo);
-    }
-    else {
-        document.getElementById("fondoNeutro").remove();
-        contenidoVideo = document.createElement("VIDEO");
-        contenidoVideo.setAttribute("class", "muestraVideo");
-        contenidoVideo.setAttribute("id", "videoActual");
-
-        if (contenidoVideo.canPlayType("video/mp4")) {
-            contenidoVideo.setAttribute("src", "videos/pexels-swapnil-sharma-14916652 (1080p).mp4");
-        } else {
-            contenidoVideo.setAttribute("src", "movie.ogg");
-        }
-
-        contenidoVideo.setAttribute("width", "720");
-        contenidoVideo.setAttribute("height", "400");
-        contenidoVideo.setAttribute("controls", "controls");
-        document.getElementById("pantallaVideo").appendChild(contenidoVideo);
-    }
-}*/
