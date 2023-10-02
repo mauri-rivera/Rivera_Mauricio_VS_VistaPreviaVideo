84beccdbae64c3d6890524da25169b0c05eabf3c
mauri-rivera <mauri.rivera@outlook.com>
2023-10-02 17:02:25 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,7 +20,6 @@ function muestraVideo(atributo) {
 
     if (contenidoVideo.canPlayType("video/mp4")) {
         let ubicacion = encontrarUbicacion(identificador);
-        console.log(ubicacion);
         contenidoVideo.setAttribute("src", ubicacion);
         contenidoVideo.setAttribute("onmouseover", "playVideo(this)");
         contenidoVideo.setAttribute("onmouseout", "stopVideo(this)");
@@ -38,36 +37,28 @@ function muestraVideo(atributo) {
 }
 
 function encontrarUbicacion(id) {
-    let ruta = "";/*, rutaVideo = "";
-    let videoId = document.querySelector(`#${id}`);
-    let hashtag = videoId.getAttribute("href");*/
+    let ruta = "";
 
     switch (id) {
         case "video1":
-            //ruta = "Rivera_Mauricio_VS_VistaPreviaVideo/videos/videos1/movie.mp4";
             ruta = "videos/videos1/movie.mp4";
             break;
         case "video2":
-            //ruta = "Rivera_Mauricio_VS_VistaPreviaVideo/videos/videos2/pexels-yogendra-singh-15135149.mp4";
             ruta = "videos/videos2/pexels-yogendra-singh-15135149.mp4";
             break;
         case "video3":
-            //ruta = "Rivera_Mauricio_VS_VistaPreviaVideo/videos/videos3/pexels-ricardo-perez-18092718.mp4";
             ruta = "videos/videos3/pexels-ricardo-perez-18092718.mp4";
             break;
         case "video4":
-            //ruta = "Rivera_Mauricio_VS_VistaPreviaVideo/videos/videos4/pexels-swapnil-sharma-14916652.mp4";
             ruta = "videos/videos4/pexels-swapnil-sharma-14916652.mp4";
             break;
     }
 
     return ruta;
-    //return rutaVideo;
 }
 
 function playVideo(idVideo) {
     let videoId = idVideo.id;
-    console.log(videoId);
     document.getElementById(videoId).play();
     document.getElementById(videoId).muted = true;
 }
